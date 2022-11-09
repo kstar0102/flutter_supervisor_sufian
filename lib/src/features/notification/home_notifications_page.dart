a1fee213b3415fd4de143b8ec93716adb894e400
JX <andrei.rd129@gmail.com>
2022-11-09 13:45:08 +0900

    group notifications by date.

--- lib/src/features/notification/home_notifications_page.dart
+++ lib/src/features/notification/home_notifications_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:intl/intl.dart';
 
 import 'package:alnabali_driver/src/features/notification/home_notifications_controller.dart';
 import 'package:alnabali_driver/src/features/notification/notif_card.dart';
@@ -46,18 +48,68 @@ class _HomeNotificationsPageState extends ConsumerState<HomeNotificationsPage> {
       child: ProgressHUD(
         inAsyncCall: state.isLoading,
         child: notis != null && notis.isNotEmpty
-            ? ListView.separated(
-                padding: EdgeInsets.only(top: 100.h),
-                itemCount: notis.length,
-                itemBuilder: (BuildContext context, int itemIdx) {
+            ? GroupedListView(
+                elements: notis,
+                groupBy: (notif) => notif.notifyDate,
+                groupSeparatorBuilder: (value) {
+                  final now = DateTime.now();
+                  final today = DateTime(now.year, now.month, now.day);
+                  final yesterday = DateTime(now.year, now.month, now.day - 1);
+                  final valueDate =
+                      DateTime(value.year, value.month, value.day);
+
+                  final String dateText;
+                  if (valueDate == today) {
+                    dateText =
+                        'Today, ${DateFormat('mm-dd-yyyy').format(value)}';
+                  } else if (valueDate == yesterday) {
+                    dateText =
+                        'Yesterday, ${DateFormat('mm-dd-yyyy').format(value)}';
+                  } else {
+                    dateText = DateFormat('E, mm-dd-yyyy').format(value);
+                  }
+                  return Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 30.h, horizontal: 100.w),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Expanded(child: Divider(color: Colors.black)),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30.w),
+                          child: Text(
+                            dateText,
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 32.sp,
+                            ),
+                          ),
+                        ),
+                        const Expanded(child: Divider(color: Colors.black)),
+                      ],
+                    ),
+                  );
+                },
+                itemBuilder: (context, element) {
                   return NotifCard(
-                    info: notis[itemIdx],
+                    info: element,
                     onPressed: () {},
                   );
                 },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(),
               )
+            // ? ListView.separated(
+            //     padding: EdgeInsets.only(top: 100.h),
+            //     itemCount: notis.length,
+            //     itemBuilder: (BuildContext context, int itemIdx) {
+            //       return NotifCard(
+            //         info: notis[itemIdx],
+            //         onPressed: () {},
+            //       );
+            //     },
+            //     separatorBuilder: (BuildContext context, int index) =>
+            //         const SizedBox(child: Text('separator')),
+            //   )
             : const SizedBox(),
       ),
     );
